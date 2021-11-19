bc8b2541e333d1ee1b4f5f64103dcb5380172ab4
Tayebe Hosseini <st.hosseini92@gmail.com>
2021-11-20 00:48:09 +0330

    changed to light

--- lib/theme/theme_manager.dart
+++ lib/theme/theme_manager.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ThemeManager with ChangeNotifier {
-  ThemeMode _themeMode = ThemeMode.dark;
+  ThemeMode _themeMode = ThemeMode.light;
 
 
   ThemeMode get themeMode => _themeMode;
